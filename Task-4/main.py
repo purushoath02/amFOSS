b6ecf5dd6dd37421f291e3c6a67e508c41ef4d81
purushoath02 <purushoath2002@gmail.com>
2022-05-12 15:03:30 +0530

    Task-4 update

--- Task-4/main.py
+++ Task-4/main.py
@@ -1,12 +1,12 @@
 
-from operator import imod
+#from operator import imod
 import string
 from urllib import response
 import typer 
 from datetime import datetime  
 import requests
 from PIL import Image
-from io import BytesIO
+#from io import BytesIO
 import json
 import urllib.request
 
@@ -32,10 +32,18 @@ def fetch_img(date: datetime, id: int):
         if x['id'] == id:
             print(x['img_src'])
             urllib.request.urlretrieve(x['img_src'],"image.png")
-
             img = Image.open("image.png")
             img.show()
                 
+def list_id(date: datetime): 
+    print("Sending API Req")
+    date = str(date.date())
+    API_URL = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={date}&api_key={API_KEY}"
+    response = requests.get(API_URL)
+    raw_data = json.loads(response.content)
+    for x in raw_data['photos']:
+        print(x['id'])
+    
 
     
   
